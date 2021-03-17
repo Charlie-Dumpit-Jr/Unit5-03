7f9744fb83071a58e057f2b2e43da6f45211fd3b
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-17 13:21:01 +0000

    Unit5-03: Functions (2)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.getElementById('results').addEventListener('click', reportResults)
 
-function reportResults() {
+function reportResults () {
   alert(document.getElementById('input').value)
 }
